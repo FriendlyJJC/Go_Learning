b75a063c529d8632808cc5c8907f8640c7795ee2
FriendlyJJC <bruck708@gmail.com>
2025-05-02 23:18:49 +0200

    Added new Lesson with Interfaces

--- Bank_Account/main.go
+++ Bank_Account/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+type Account interface {
+	add(value int) int
+	decrease(value int) int
+	get() int
+}
+
+type AccountBalance struct {
+	balance int
+}
+
+func (b *AccountBalance) add(value int) int {
+	b.balance += value
+	return b.balance
+}
+
+func (b *AccountBalance) decrease(value int) int {
+	b.balance -= value
+	return b.balance
+}
+
+func (b *AccountBalance) get() int {
+	return b.balance
+}
+
+func main() {
+	var balance Account = &AccountBalance{balance: 0}
+	fmt.Println(balance.get())
 }
