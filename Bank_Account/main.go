7d34ad3b986b8f6575e02b8a12a9c7deed0893f0
FriendlyJJC <bruck708@gmail.com>
2025-05-02 23:33:08 +0200

    updated Comments and some functions

--- Bank_Account/main.go
+++ Bank_Account/main.go
@@ -2,27 +2,35 @@ package main
 
 import "fmt"
 
+//Initialize Account Type with Functions
+
 type Account interface {
 	add(value int) int
 	decrease(value int) int
 	get() int
 }
 
+//Initialize Accountbalance with balance as a varialbe
+
 type AccountBalance struct {
 	balance int
 }
+
+//The Account Function add with a pointer to the Accountbalance to reference the Value
 
 func (b *AccountBalance) add(value int) int {
 	b.balance += value
 	return b.balance
 }
 
+// The Account Function decrease with a pointer to the Accountbalance to reference the Value
 func (b *AccountBalance) decrease(value int) int {
 	b.balance -= value
 	return b.balance
 }
 
-func (b *AccountBalance) get() int {
+// The Account Function add with a pointer to the Accountbalance to reference the Value
+func (b AccountBalance) get() int {
 	return b.balance
 }
 
